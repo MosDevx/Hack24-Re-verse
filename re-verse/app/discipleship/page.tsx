a9cc14cd1181692ee4f60afbe9b94d759fc4f216
MosDevx <mosesk.wamae@gmail.com>
2024-10-26 23:57:42 +0300

    discipleship

--- re-verse/app/discipleship/page.tsx
+++ re-verse/app/discipleship/page.tsx
@@ -3,10 +3,42 @@ import Image from "next/image";
 import { CourseTabs } from "@/components/ui/course-tabs";
 
 // Define the tabs variable
+
+
+const DummyContent = () => {
+  return (
+    <Image
+      src="/linear.webp"
+      alt="dummy image"
+      width="1000"
+      height="1000"
+      className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
+    />
+  );
+};
 const tabs = [
-	{ id: 1, title: "Tab 1", content: "Content for Tab 1" },
-	{ id: 2, title: "Tab 2", content: "Content for Tab 2" },
-	{ id: 3, title: "Tab 3", content: "Content for Tab 3" },
+	{
+		title: "Product",
+		value: "product",
+		content: (
+			<div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+				<p>Product Tab</p>
+				<DummyContent />
+			</div>
+		),
+	},
+
+	{
+		title: "Services",
+		value: "services",
+		content: (
+			<div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+				<p>Services tab</p>
+				<DummyContent />
+			</div>
+		),
+	},
+	
 ];
 
 export default function Home() {
@@ -14,7 +46,10 @@ export default function Home() {
 		<>
 			<div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full items-start justify-start my-40">
 				<CourseTabs tabs={tabs} />
-			</div>
+			</div>	
 		</>
 	);
 }
+
+
+

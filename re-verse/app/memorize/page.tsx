47739fcf25b3d3c08bbe5f80743a8fe48997893a
MosDevx <mosesk.wamae@gmail.com>
2024-11-15 18:49:06 +0300

    versessArray

--- re-verse/app/memorize/page.tsx
+++ re-verse/app/memorize/page.tsx
@@ -1,4 +1,4 @@
-// import { prisma } from '@/lib/db'
+import { prisma } from '@/lib/db'
 
 import VerseDisplay from '@/components/ui/Memorize/verse-display'
 
@@ -6,13 +6,15 @@ export default async function Memorize(){
 
 
 
-	// const verses = await prisma.verse.findMany()
+	const versesArray = await prisma.verse.findMany()
 
 	return(
 
 		<div className="flex flex-col items-center bg-red-100 justify-center min-h-screen py-10 px-4 lg:px-20">
 			<h1>MEMORIZE</h1>
-			<VerseDisplay reference="John 3:16" text="for God so loved the world"/>
+			
+			
+			<VerseDisplay  versesArray={versesArray}/>
 
 			<button className="bg-blue-500 text-white p-2 rounded-lg mt-2 self-end"
 			>Skip</button>
--- re-verse/components/ui/Memorize/verse-display.tsx
+++ re-verse/components/ui/Memorize/verse-display.tsx
@@ -1,12 +1,14 @@
 
 "use client"
 
-export default function VerseDisplay({reference,text}:{reference:string, text:string}){
+export default function VerseDisplay({versesArray}:{versesArray:[]}){
 
+
+		console.log(versesArray)
 	return(
 
 		<div className="flex flex-col items-center justify-center  py-2 px-4 lg:px-20 bg-gray-50">
-			<h1>{reference}</h1>
+			<h1></h1>
 
 			<input type="text" className="border-2 border-gray-300 p-2 rounded-lg w-full" placeholder="Type the verse here" />
 

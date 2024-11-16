c79c67dc6a17561144cada28ee7fe6bf0c1d3b02
MosDevx <mosesk.wamae@gmail.com>
2024-11-16 17:31:18 +0300

    Merge remote-tracking branch 'origin/reverse-ui' into merging-branch

--- re-verse/app/memorize/page.tsx
+++ re-verse/app/memorize/page.tsx
@@ -9,17 +9,19 @@ export default async function Memorize(){
 	const versesArray = await prisma.verse.findMany()
 
 	return(
-
-		<div className="flex flex-col items-center bg-red-100 justify-center min-h-screen py-10 px-4 lg:px-20">
-			<h1>MEMORIZE</h1>
+		<>
+		<div className="flex flex-col items-center bg-red-100 h-screen  min-h-screen py-10 px-4 lg:px-20">
 			
-			
+		<h1>MEMORIZE</h1>
+			<div className='w-full grow flex flex-col h-full justify-center items-center bg-yellow-400'>
 			<VerseDisplay  versesArray={versesArray}/>
 
-			<button className="bg-blue-500 text-white p-2 rounded-lg mt-2 self-end"
-			>Skip</button>
+			</div>
+
+
 
 		</div>
+		</>
 	)
 
 

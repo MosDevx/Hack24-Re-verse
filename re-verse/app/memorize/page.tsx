a53aeea8a440f122a87e21898699b31ecd761ce4
MosDevx <mosesk.wamae@gmail.com>
2024-11-17 14:01:24 +0300

    implement refresh game

--- re-verse/app/memorize/page.tsx
+++ re-verse/app/memorize/page.tsx
@@ -10,15 +10,13 @@ export default async function Memorize(){
 
 	return(
 		<>
-		<div className="flex flex-col items-center bg-red-100 h-screen  min-h-screen py-10 px-4 lg:px-20">
+		<div className="flex flex-col items-center h-screen min-h-screen py-10 px-4 lg:px-20">
 			
-		<h1>MEMORIZE</h1>
-			<div className='w-full grow flex flex-col h-full justify-center items-center bg-yellow-400'>
-			<VerseDisplay  versesArray={versesArray}/>
+		<h1 className="text-4xl font-bold text-black mb-8">MEMORIZE</h1>
+			<div className='w-full grow flex flex-col h-full justify-center items-center bg-gray-800'>
+			<VerseDisplay versesArray={versesArray} />
 
 			</div>
-
-
 
 		</div>
 		</>
--- re-verse/components/ui/Memorize/verse-switcher.tsx
+++ re-verse/components/ui/Memorize/verse-switcher.tsx
@@ -8,12 +8,13 @@ interface VerseSwitcherProps {
   versesArray: { reference: string }[];
   handleCheck: () => void;
   showDiff: boolean;
+  handleKeyDown: (event: React.KeyboardEvent<HTMLTextAreaElement>) => void;
   correctVerse: string;
   userVerse: string;
   inputRef: React.RefObject<HTMLInputElement>;
 }
 
-export default function VerseSwitcher({currentIndex, versesArray, handleCheck, showDiff, correctVerse, userVerse, inputRef}: VerseSwitcherProps){
+export default function VerseSwitcher({currentIndex, versesArray, handleCheck, handleKeyDown, showDiff, correctVerse, userVerse, inputRef}: VerseSwitcherProps){
 
 
 return(
@@ -34,19 +35,29 @@ return(
 		}}
 	>
 	
+	<div className="flex flex-col items-center justify-center mt-12 bg-white shadow-md rounded-lg py-6 px-8 lg:px-24">
+		<h1 className="text-2xl font-semibold mb-4">{versesArray[currentIndex].reference}</h1>
 
-	<div className="flex flex-col items-center justify-center bg-red-100 py-2 px-4 lg:px-20 ">
-		<h1>{versesArray[currentIndex].reference}</h1>
+		{showDiff && (
+			<CompareStrings
+				correctVerse={correctVerse}
+				userInput={userVerse || ''}
+			/>
+		)}
 
-	{showDiff &&	<CompareStrings
-			correctVerse={correctVerse}
-			userInput={userVerse || ''}/>}
-
-		<input type="text" className="border-2 border-gray-300 p-2 rounded-lg w-full" ref={inputRef} placeholder="Type the verse here" />
-		<button className="bg-blue-500 text-white p-2 rounded-lg mt-2" onClick={handleCheck}>
+		<textarea
+			type="text"
+			className="border-2 border-gray-300 p-3 rounded-lg w-full mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+			ref={inputRef}
+			placeholder="Type the verse here"
+			onKeyDown={handleKeyDown}
+		/>
+		<button
+			className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300"
+			onClick={handleCheck}
+		>
 			Check
 		</button>
-	
 	</div>
 
 	</motion.div>

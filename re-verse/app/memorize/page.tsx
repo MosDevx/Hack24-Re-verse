603cc7d0db58ee3485846c79649ae30ce5d5d69f
MosDevx <mosesk.wamae@gmail.com>
2024-11-18 02:21:57 +0300

    fix memorize

--- re-verse/app/memorize/page.tsx
+++ re-verse/app/memorize/page.tsx
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/db'
+// import { prisma } from '@/lib/db'
 
 import VerseDisplay from '@/components/ui/Memorize/verse-display'
 
@@ -6,7 +6,30 @@ export default async function Memorize(){
 
 
 
-	const versesArray = await prisma.verse.findMany()
+	// const versesArray = await prisma.verse.findMany()
+
+	const versesArray = [
+		{
+			id: 1,
+			reference: 'John 3:16',
+			content: 'For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.'
+		},
+		{
+			id: 2,
+			reference: 'John 1:1',
+			content: 'In the beginning was the Word, and the Word was with God, and the Word was God.'
+		},
+		{
+			id: 3,
+			reference: 'John 1:14',
+			content: 'The Word became flesh and made his dwelling among us. We have seen his glory, the glory of the one and only Son, who came from the Father, full of grace and truth.'
+		},
+		{
+			id: 4,
+			reference: 'John 14:6',
+			content: 'Jesus answered, "I am the way and the truth and the life. No one comes to the Father except through me."'
+		},
+	]
 
 	return(
 		<>

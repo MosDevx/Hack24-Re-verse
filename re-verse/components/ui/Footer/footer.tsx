936b36a9295cfb86fd451a733fd8dc0627e5e089
WilliamNyamu <williamnyamu08@gmail.com>
2024-11-17 22:34:14 +0300

    Watermark changes

--- re-verse/components/ui/Footer/footer.tsx
+++ re-verse/components/ui/Footer/footer.tsx
@@ -3,6 +3,10 @@ import React from "react";
 import { AnimatedTooltip } from "./animated-tooltip";
 import Loice from "@/public/Image/Loice.jpg";
 import Billy from "@/public/Image/Billy.png";
+import Jeff from "@/public/Image/Jeff.jpg";
+import Moses from "@/public/Image/Moses.jpg";
+import Michael from "@/public/Image/Michael.jpg";
+import Peninah from "@/public/Image/Peninnah.jpg";
 import { StaticImageData } from "next/image";
 
 type Person = {
@@ -21,37 +25,45 @@ const people: Person[] = [
   },
   {
     id: 2,
-    name: "Robert Johnson",
-    designation: "Product Manager",
-    image:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    name: "George",
+    designation: "Strategist",
+    image: Loice,
   },
   {
     id: 3,
-    name: "Jane Smith",
-    designation: "Data Scientist",
-    image:
-      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    name: "Peninah",
+    designation: "Strategist",
+    image: Peninah,
   },
   {
     id: 4,
-    name: "Emily Davis",
-    designation: "UX Designer",
-    image:
-      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    name: "Michael",
+    designation: "Business Analyst",
+    image: Michael,
   },
   {
     id: 5,
+    name: "Jeff",
+    designation: "Software Developer",
+    image: Jeff,
+  },
+  {
+    id: 6,
+    name: "Daniel",
+    designation: "Software Developer",
+    image: Jeff,
+  },
+  {
+    id: 7,
     name: "Billy",
     designation: "Software Developer",
     image: Billy,
   },
   {
-    id: 6,
-    name: "Dora",
-    designation: "The Explorer",
-    image:
-      "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+    id: 8,
+    name: "Moses",
+    designation: "Project Lead",
+    image: Moses,
   },
 ];
 
